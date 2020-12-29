9cac23dee44ccffa6427ef0dcc41078723b45285
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2020-12-29 19:15:33 +0530

    little debuging

--- src/backend.js
+++ src/backend.js
@@ -1,6 +1,6 @@
 // API's
-// export const API = "https://dountubeapi.herokuapp.com";
-export const API = "http://localhost:7000";
+export const API = "https://dountubeapi.herokuapp.com";
+// export const API = "http://localhost:7000";
 
 // for prepare the title url friendly
 export const Prepairtitle = (title) =>{
